f0a126c3ff367ddadbe614df488d7aaa35b7fca7
RyanQchiqache <ryanqchiqache16@gmail.com>
2025-08-01 15:04:42 +0200

    made 2 seperate files for preprocessing

---
+++ computerVisionBach/models/Unet_SS/preprocessing/dlr_preprocessing.py
@@ -0,0 +1,132 @@
+import os
+import cv2
+import sys
+import numpy as np
+import torch
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+from computerVisionBach.models.Unet_SS.satellite_dataset.satellite_data import SatelliteDataset
+from computerVisionBach.models.Unet_SS import visualisation
+from transformers import SegformerImageProcessor
+from torchvision import transforms as T
+from computerVisionBach.models.Unet_SS import utils
+processor = SegformerImageProcessor.from_pretrained("nvidia/segformer-b2-finetuned-ade-512-512")
+patch_size, overlap = 512, 0.5
+transforms = T.Compose([
+    T.ToTensor(),
+    T.RandomHorizontalFlip(p=0.5),
+    T.RandomVerticalFlip(p=0.5),
+    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+FLAIR_USED_LABELS = [1, 2, 3, 6, 7, 8, 10, 11, 13, 18]
+
+# =====================================
+# patchify and load data DLR skyscapes
+# =====================================
+
+def patchify_image_or_masks(image_mask: np.ndarray, patch_size, overlap: float):
+    patch_size = patch_size
+    step = int(patch_size * (1 - overlap))
+    H, W = image_mask.shape[:2]
+
+    pad_bottom = (patch_size - H % patch_size) % patch_size
+    pad_right = (patch_size - W % patch_size) % patch_size
+
+
+    if pad_bottom or pad_right:
+        constant_values = 255 if image_mask.ndim == 2 else 0
+        pad_shape = ((0, pad_bottom), (0, pad_right), (0, 0)) if image_mask.ndim == 3 else ((0, pad_bottom),
+                                                                                                (0, pad_right))
+        image = np.pad(image_mask, pad_shape, mode="constant", constant_values=constant_values)
+        H, W = image.shape[:2]
+    else:
+        image = image_mask
+
+    patches = []
+    for top in range(0, H - patch_size + 1, step):
+        for left in range(0, W - patch_size + 1, step):
+            patch = image[top:top + patch_size, left:left + patch_size]
+            patches.append(patch)
+
+    return patches, (H, W)
+
+
+def load_folder(image_dir, mask_dir=None, patchify_enabled:bool=True):
+    images = sorted([os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith('.jpg') or f.endswith('.png')])
+    X, y = [], []
+    if mask_dir:
+        masks = sorted([os.path.join(mask_dir, f) for f in os.listdir(mask_dir) if f.endswith('.png')])
+        for img_path, mask_path in zip(images, masks):
+            img = cv2.imread(img_path).astype(np.uint8)
+            mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE).astype(np.uint8)
+
+            unique_classes = np.unique(mask)
+            print(f"{os.path.basename(mask_path)} → Classes present: {unique_classes}")
+
+            if patchify_enabled:
+                image_p, _ = patchify_image_or_masks(img, patch_size, overlap)
+                mask_p, _ = patchify_image_or_masks(mask, patch_size, overlap)
+                X.extend(image_p)
+                y.extend(mask_p)
+
+
+            else:
+                X.append(img)
+                y.append(mask)
+
+        return X, y
+    else:
+        for img_path in images:
+            img = cv2.imread(img_path)
+            if patchify_enabled:
+                image_p, _ = patchify_image_or_masks(img, patch_size, overlap)
+                X.extend(image_p)
+            else:
+                X.append(img)
+
+        return X
+
+def load_data_dlr(base_dir, dataset_type="SS_Dense"):
+    base = os.path.join(base_dir, dataset_type)
+
+    X_train, y_train = load_folder(
+        os.path.join(base, "train/images"),
+        os.path.join(base, "train/labels/grayscale")
+    )
+    X_val, y_val = load_folder(
+        os.path.join(base, "val/images"),
+        os.path.join(base, "val/labels/grayscale")
+    )
+    X_test = load_folder(
+        os.path.join(base, "test/images"),
+        mask_dir=None
+    )
+
+    color_map_rgb = {k: utils.hex_to_rgb(v[1]) for k, v in utils.COLOR_MAP_dense.items()}
+
+    visualisation.visualize_sample(
+        X_train,
+        [utils.class_to_rgb(mask, color_map_rgb) for mask in y_train],
+        y_train
+    )
+    ## relabel 1–20 → 0–19, and 0 → 255 (ignored)
+    def relabel_fn(mask):
+        relabeled = np.full_like(mask, 255, dtype=np.int64)  # default to ignore index
+        valid = (mask >= 1) & (mask <= 20)
+        relabeled[valid] = mask[valid] - 1  # remap 1–20 → 0–19
+        return relabeled
+
+    train_dataset = SatelliteDataset(X_train, y_train, transform=transforms, relabel_fn=relabel_fn)
+    val_dataset = SatelliteDataset(X_val, y_val, transform=transforms, relabel_fn=relabel_fn)
+    test_dataset = SatelliteDataset(X_test, masks=None)
+
+    num_classes = 20  # or whatever you set in the model
+    """for i, (_, mask) in enumerate(train_dataset):
+        uniques = torch.unique(mask)
+        print(f"Sample {i}: Unique labels:", uniques.tolist())
+        valid = (uniques == 255) | ((uniques >= 0) & (uniques < num_classes))
+        assert torch.all(valid), f"Invalid label(s) in sample {i}: {uniques.tolist()}"""
+
+    return train_dataset, val_dataset, test_dataset
+
+
---
+++ computerVisionBach/models/Unet_SS/preprocessing/flair_preprocessing.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import numpy as np
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+from typing import Callable, Tuple, List
+import csv
+from computerVisionBach.models.Unet_SS.satellite_dataset.flair_dataset import FlairDataset
+from transformers import SegformerImageProcessor
+from torchvision import transforms as T
+processor = SegformerImageProcessor.from_pretrained("nvidia/segformer-b2-finetuned-ade-512-512")
+
+transforms = T.Compose([
+    T.RandomHorizontalFlip(p=0.5),
+    T.RandomVerticalFlip(p=0.5),
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+FLAIR_USED_LABELS = [1, 2, 3, 6, 7, 8, 10, 11, 13, 18]
+
+
+# =====================================
+# patchify and load data FLAIR
+# =====================================
+def prepare_datasets_from_csvs(
+    train_csv_path: str,
+    val_csv_path: str,
+    test_csv_path: str,
+    base_dir: str = None
+) -> Tuple[FlairDataset, FlairDataset, FlairDataset]:
+    def load_csv(csv_path: str) -> List[Tuple[str, str]]:
+        with open(csv_path, newline='') as f:
+            reader = csv.reader(f)
+            return [(row[0], row[1]) for row in reader if len(row) == 2]
+
+    def resolve_path(p: str) -> str:
+        return os.path.normpath(os.path.join(base_dir, p)) if base_dir and not os.path.isabs(p) else p
+
+    # Load CSVs
+    train_pairs = load_csv(train_csv_path)
+    val_pairs = load_csv(val_csv_path)
+    test_pairs = load_csv(test_csv_path)
+
+    train_pairs = [(resolve_path(img), resolve_path(mask)) for img, mask in train_pairs]
+    val_pairs = [(resolve_path(img), resolve_path(mask)) for img, mask in val_pairs]
+    test_pairs = [(resolve_path(img), resolve_path(mask)) for img, mask in test_pairs]
+
+    train_imgs, train_masks = zip(*train_pairs)
+    val_imgs, val_masks = zip(*val_pairs)
+    test_imgs, test_masks = zip(*test_pairs)
+
+    relabel_fn = lambda mask: relabel_mask(mask, original_labels=FLAIR_USED_LABELS)
+    train_dataset = FlairDataset(train_imgs, train_masks, transform=transforms)
+    val_dataset = FlairDataset(val_imgs, val_masks,transform=transforms)
+    test_dataset = FlairDataset(test_imgs, test_masks, transform=transforms)
+
+    return train_dataset, val_dataset, test_dataset
+
+def relabel_mask(mask: np.ndarray, original_labels: list) -> np.ndarray:
+    """
+    Remaps original sparse label values (e.g., [1, 2, 6, 18]) to contiguous [0, 1, 2, ..., N-1]
+    so CrossEntropyLoss works without index errors.
+    """
+    label_map = {orig: new for new, orig in enumerate(sorted(original_labels))}
+    remapped = np.zeros_like(mask)
+    for orig_label, new_label in label_map.items():
+        remapped[mask == orig_label] = new_label
+    return remapped
