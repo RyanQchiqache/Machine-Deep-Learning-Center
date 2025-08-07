0f09d3a37b77f77aa47d1e5dfecf9d7bf156fb5f
RyanQchiqache <ryanqchiqache16@gmail.com>
2025-08-07 10:24:42 +0200

    preprocessing with no test dataset for now

--- computerVisionBach/models/Unet_SS/preprocessing/flair_preprocessing.py
+++ computerVisionBach/models/Unet_SS/preprocessing/flair_preprocessing.py
@@ -3,19 +3,29 @@ import sys
 import numpy as np
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
-from typing import Callable, Tuple, List
+from typing import Callable, Tuple, List, Optional
 import csv
 from computerVisionBach.models.Unet_SS.satellite_dataset.flair_dataset import FlairDataset
 from transformers import SegformerImageProcessor
 from torchvision import transforms as T
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 processor = SegformerImageProcessor.from_pretrained("nvidia/segformer-b2-finetuned-ade-512-512")
 
-transforms = T.Compose([
-    T.RandomHorizontalFlip(p=0.5),
-    T.RandomVerticalFlip(p=0.5),
-    T.ToTensor(),
-    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+tf_train = A.Compose([
+    A.HorizontalFlip(p=0.5),
+    A.RandomRotate90(p=0.5),
+    A.VerticalFlip(p=0.5),
+    A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ToTensorV2()
 ])
+
+tf_val = A.Compose([
+    A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ToTensorV2()
+])
+
+
 FLAIR_USED_LABELS = [1, 2, 3, 6, 7, 8, 10, 11, 13, 18]
 
 
@@ -25,7 +35,7 @@ FLAIR_USED_LABELS = [1, 2, 3, 6, 7, 8, 10, 11, 13, 18]
 def prepare_datasets_from_csvs(
     train_csv_path: str,
     val_csv_path: str,
-    test_csv_path: str,
+    test_csv_path: Optional[str] = None,
     base_dir: str = None
 ) -> Tuple[FlairDataset, FlairDataset, FlairDataset]:
     def load_csv(csv_path: str) -> List[Tuple[str, str]]:
@@ -39,20 +49,27 @@ def prepare_datasets_from_csvs(
     # Load CSVs
     train_pairs = load_csv(train_csv_path)
     val_pairs = load_csv(val_csv_path)
-    test_pairs = load_csv(test_csv_path)
+
 
     train_pairs = [(resolve_path(img), resolve_path(mask)) for img, mask in train_pairs]
     val_pairs = [(resolve_path(img), resolve_path(mask)) for img, mask in val_pairs]
-    test_pairs = [(resolve_path(img), resolve_path(mask)) for img, mask in test_pairs]
 
     train_imgs, train_masks = zip(*train_pairs)
     val_imgs, val_masks = zip(*val_pairs)
-    test_imgs, test_masks = zip(*test_pairs)
 
     relabel_fn = lambda mask: relabel_mask(mask, original_labels=FLAIR_USED_LABELS)
-    train_dataset = FlairDataset(train_imgs, train_masks, transform=transforms)
-    val_dataset = FlairDataset(val_imgs, val_masks,transform=transforms)
-    test_dataset = FlairDataset(test_imgs, test_masks, transform=transforms)
+
+    train_dataset = FlairDataset(train_imgs, train_masks, transform=tf_train, relabel_fn=relabel_fn)
+    val_dataset = FlairDataset(val_imgs, val_masks,transform=tf_val, relabel_fn=relabel_fn)
+
+    if test_csv_path is not None:
+        test_pairs = load_csv(test_csv_path)
+        test_pairs = [(resolve_path(img), resolve_path(mask)) for img, mask in test_pairs]
+        test_imgs, test_masks = zip(*test_pairs)
+        test_dataset = FlairDataset(test_imgs, test_masks, transform=tf_val)
+
+    else:
+        test_dataset = None
 
     return train_dataset, val_dataset, test_dataset
 
